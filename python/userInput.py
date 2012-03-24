e73db9213013ed58a44eca46a16cf68efbc16456
Sam Kohn <therunner05@gmail.com>
2012-03-24 18:33:18 -0400

    added addSong() and removeSong() to userInput.py

--- python/userInput.py
+++ python/userInput.py
@@ -4,4 +4,44 @@ import pymongo
 import sys
 import re
 
-connection = pymongo.Connection("localhost")
+def testMongoDB():
+    connection = pymongo.Connection("chipper.bu.edu")
+    db = connection.hackny
+    db.authenticate("hackny","hackme")
+    collection = db.hackNYTest
+    collection.insert({'test': 'test'})
+    collection.insert({'test': 'test2'})
+    print(collection.find()[0])
+    print(collection.find()[1])
+
+    collection.remove({'test': 'test'})
+    collection.remove({'test': 'test2'})
+
+def getDB():
+    connection = pymongo.Connection("chipper.bu.edu")
+    db = connection.hackny
+    db.authenticate("hackny","hackme")
+    collection = db.hackNYTest
+    return collection
+
+# Adds the given song to the db returned by getDB()
+def addSong(youTubeURL, songTitle, songArtist):
+    collection = getDB()
+    song = {'URL': youTubeURL, 'Title': songTitle, 'Artist': songArtist}
+    print(song)
+    collection.insert(song)
+    print('inserted song')
+    print(collection.find(song)[0])
+    
+# Removes the given song from the db returned by getDB()
+def removeSong(youTubeURL, songTitle, songArtist):
+    collection = getDB()
+    song = {'URL': youTubeURL, 'Title': songTitle, 'Artist': songArtist}
+    print(song)
+    collection.remove(song)
+    print('removed song')
+
+addSong('http://www.mozart.com','Sonata','Mozart')
+addSong('http://www.beethoven.com','Concerto','Beethoven')
+removeSong('http://www.mozart.com','Sonata','Mozart')
+print(getDB().find_one())
