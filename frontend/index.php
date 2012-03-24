c66d7d9aa0356bb3dd5610633781ffa3258d2d84
John-Nicholas Furst <J-N@John-Nicholas.Net>
2012-03-24 19:35:32 -0400

    frontend

---
+++ frontend/index.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+<center>
+Yo give me a youtube hash: <br/><br/>
+<form method='POST' action'#'>
+<input type='text' name='ytHash'/>
+<input type='submit' value='Submit that shit'>
+</form>
+</center>
+</body>
+</head>
