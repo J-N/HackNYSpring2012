101faab91757596e44e0b523027491ebcc6c8ad4
Raymond Ehlers <raymond.ehlers@gmail.com>
2012-03-25 04:50:55 -0400

    Commented code (Gasp!)

--- python/parselyAPI.py
+++ python/parselyAPI.py
@@ -18,13 +18,16 @@ def getLyrics(artist, title):
     if (a.isalnum() == True):
       urlTitle += a
   url = urlBase + urlArtist + "/" + urlTitle + ".html"
-  print(url)
+  #print(url)
+  # GET request to azlyrics
   response = urllib2.urlopen(url)
   html = response.read()
   #print(html)
   return html
   
 def removeCrapFromLyrics(lyrics, regex):
+  # Uses regex to remove html tags, as well as words inside of [], which
+  # break parsely and usually contain useless information like 'Chorus'
   cleanFlag = False
   while (cleanFlag == False):
     match = re.search(regex, lyrics)
@@ -39,6 +42,7 @@ def removeCrapFromLyrics(lyrics, regex):
   return lyrics
 
 def parselyLyrics(lyrics):
+  # Creates url to GET with parsely
   parselyUrl = "http://simon.parsely.com:8983/solr/goldindex2/select/?wt=json&"
   lyricsEncode = { "q" : lyrics }
   lyricsQuery = urllib.urlencode(lyricsEncode)
@@ -49,9 +53,12 @@ def parselyLyrics(lyrics):
 def parseLyrics(artist, title):
   source = getLyrics(artist, title)
   result = source.find("<!-- start of lyrics -->")
+  # Takes the first 250 characters, including crap we don't need like
+  # html tags and [] that are removed after
   lyrics = source[result+24:result+274]
   lyrics = removeCrapFromLyrics(lyrics, "\[.*?\]")
   lyrics = removeCrapFromLyrics(lyrics, "<.*?>")
+  # Use parsely to find headlines
   parselyUrl = parselyLyrics(lyrics)
   response = urllib2.urlopen(parselyUrl)
   html = response.read()
