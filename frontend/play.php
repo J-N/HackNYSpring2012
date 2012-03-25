87183800c0939537615f4da02f93df0df83555af
John-Nicholas Furst <J-N@John-Nicholas.Net>
2012-03-25 11:37:29 -0400

    update

--- frontend/play.php
+++ frontend/play.php
@@ -1,6 +1,19 @@
 <?php
 session_start();
 include('../phpShit/common.php');
+?>
+
+<html>
+<head>
+ <link href="http://hackerleague.org/assets/application-da9448e0ee4efa1d7b1830eec79bd41e.css" media="all" rel="stylesheet" type="text/css" />
+<script type='text/javascript' src="http://player.longtailvideo.com/jwplayer.js"></script>
+<script src="http://hackerleague.org/assets/application-8c2b636f01d3b7ed1480810e93baaa2c.js" type="text/javascript"></script>
+
+
+</head>
+<body>
+<div id="shit" style="display:none;">
+<?php
 //if(isset($_GET['g']))
 //{
 	class song{
@@ -115,12 +128,33 @@ $game = new game($_SESSION['username'],$_SESSION['gamename'],$_SESSION['hashes']
 //}
 
 ?>
+</div>
+<div class="float_left">
+<div id="player"></div>
+<div class="final_countdown">
+	<div class="value">
+            <div><span class="seconds large">00</span></div>
+            <div><span class="milliseconds large">00</span></div>
+        </div>
+        <div class="key">
+            <div><span>Seconds</span></div>
+            <div><span>Miliseconds</span></div>
+        </div>
+</div>
+</div>
+<div id="brand_text" class="float_right" style="text-align: center;
+position: relative;
+left: -25%;
+font-size: 160%;">
+<form>
 
-<html>
-<head>
- <link href="http://hackerleague.org/assets/application-da9448e0ee4efa1d7b1830eec79bd41e.css" media="all" rel="stylesheet" type="text/css" />
-<script type='text/javascript' src="http://player.longtailvideo.com/jwplayer.js"></script>
-<script src="http://hackerleague.org/assets/application-8c2b636f01d3b7ed1480810e93baaa2c.js" type="text/javascript"></script>
+<div id="1"><input id="1" type="radio" name="guess" value="1"/ ><?php $game->getSong1()->getWrong1()->printFull(); ?></div>
+<div id="2"><input id="2" type="radio" name="guess" value="2" /><?php $game->getSong1()->getWrong2()->printFull(); ?></div>
+<div id="3"><input id="3" type="radio" name="guess" value="3" /><?php $game->getSong1()->getWrong3()->printFull(); ?></div>
+<div id="4"><input id="4" type="radio" name="guess" value="4" /><?php  $game->getSong1()->printFull(); ?></div>
+</div>
+
+</body>
 
 <script type='text/javascript'>//<![CDATA[ 
 window.onload=function(){
@@ -156,44 +190,14 @@ window.onload=function(){
 	jwplayer().onPlay( function() {  
 	var clock = $('.final_countdown .value');
 	Application.timer.countdown_from(clock, 10);
-});
+//});
 	//jwplayer().onPause( function() { this.play(); });
-	//jwplayer().onComplete( function() { alert("the video is finished!"); });
+//jwplayer().onComplete( function() { 
+	if(document.getElementById('4').checked)
+	{
+		alert("you got it right");
+	}	});
 }//]]>  
-$(document).ready(function(){
-	//var clock = $('.final_countdown .value');
-	//Application.timer.countdown_from(clock, 12);
-});
 	
 </script>
-
-</head>
-<body>
-<div class="float_left">
-<div id="player"></div>
-<div class="final_countdown">
-	<div class="value">
-            <div><span class="seconds large">00</span></div>
-            <div><span class="milliseconds large">00</span></div>
-        </div>
-        <div class="key">
-            <div><span>Seconds</span></div>
-            <div><span>Miliseconds</span></div>
-        </div>
-</div>
-</div>
-<div id="brand_text" class="float_right" style="text-align: center;
-position: relative;
-left: -25%;
-font-size: 160%;">
-<form>
-
-<div id="1"><input type="radio" name="guess" value="1"/ ><?php $game->getSong1()->getWrong1()->printFull(); ?></div>
-<div id="2"><input type="radio" name="guess" value="2" /><?php $game->getSong1()->getWrong2()->printFull(); ?></div>
-<div id="3"><input type="radio" name="guess" value="3" /><?php $game->getSong1()->getWrong3()->printFull(); ?></div>
-<div id="4"><input type="radio" name="guess" value="4" /><?php  $game->getSong1()->printFull(); ?></div>
-</div>
-
-</body>
-
 </html>
