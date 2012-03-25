30daa7bc29ffd2d00687cc4253ea4fc2a1294323
Raymond Ehlers <raymond.ehlers@gmail.com>
2012-03-25 04:38:51 -0400

    Fixed further code for integration

--- python/pullAPI.py
+++ python/pullAPI.py
@@ -5,11 +5,11 @@ import parselyAPI
 def pullAPIData(artist, title):
   nytData = nytAPI.nytimesData(artist)
   parselyData = parselyAPI.parseLyrics(artist, title)
-  print(nytData)
-  print(parselyData)
+  #print(nytData)
+  #print(parselyData)
   data = [ nytData[0], nytData[1], parselyData ]
   return data
 
-artist = "Silversun Pickups"
-title = "The Royal We"
-pullAPIData(artist, title)
+#artist = "Silversun Pickups"
+#title = "The Royal We"
+#pullAPIData(artist, title)
