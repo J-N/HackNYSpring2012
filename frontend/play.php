28d0d8853a90d4e51e19cf018aa4f48bacad6092
John-Nicholas Furst <J-N@John-Nicholas.Net>
2012-04-12 10:32:20 -0400

    adding last minute changes

--- frontend/play.php
+++ frontend/play.php
@@ -6,16 +6,13 @@ include('../phpShit/common.php');
 <html>
 <head>
  <link href="http://hackerleague.org/assets/application-da9448e0ee4efa1d7b1830eec79bd41e.css" media="all" rel="stylesheet" type="text/css" />
-<script type='text/javascript' src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" />
 <script type='text/javascript' src="http://player.longtailvideo.com/jwplayer.js"></script>
 <script src="http://hackerleague.org/assets/application-8c2b636f01d3b7ed1480810e93baaa2c.js" type="text/javascript"></script>
 
+<script type='text/javascript' src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" /></script>
 
 </head>
 <body>
-<script type="text/javascript">
-	$("#test").text("Hello world");
-</script>
 <div id="test"></div>
 <div id="shit" style="display:none;">
 <?php
@@ -43,12 +40,12 @@ include('../phpShit/common.php');
 			$this->artist = $_SESSION['artists'][$num];
 			//$this->title = getTitle($theHash);
 			//$this->artist = getArtist($theHash);
-			$this->wrong1 = new song(getWrongTitle($this->hash,1),getWrongArtist($this->hash,1));
-			//$this->wrong1 = new song("Title","Artist");
-			//$this->wrong2 = new song("Title2","Artist2");
-			//$this->wrong3 = new song("Title3","Artist3");
-			$this->wrong2 = new song(getWrongTitle($this->hash,2),getWrongArtist($this->hash,2));
-			$this->wrong3 = new song(getWrongTitle($this->hash,3),getWrongArtist($this->hash,3));
+			//$this->wrong1 = new song(getWrongTitle($this->hash,1),getWrongArtist($this->hash,1));
+			$this->wrong1 = new song("Pump It","Black Eyed Peas");
+			$this->wrong2 = new song("Money In The Bank","Swizz Beatz");
+			$this->wrong3 = new song("Theres A War Going On For Your Mind","Flobots");
+			//$this->wrong2 = new song(getWrongTitle($this->hash,2),getWrongArtist($this->hash,2));
+			//$this->wrong3 = new song(getWrongTitle($this->hash,3),getWrongArtist($this->hash,3));
 			}
 			else
 			{
@@ -153,15 +150,18 @@ left: -25%;
 font-size: 160%;">
 <form>
 
-<div id="1"><input id="r1" type="radio" name="guess" value="1"/ ><?php $game->getSong1()->getWrong1()->printFull(); ?></div>
-<div id="2"><input id="r2" type="radio" name="guess" value="2" /><?php $game->getSong1()->getWrong2()->printFull(); ?></div>
-<div id="3"><input id="r3" type="radio" name="guess" value="3" /><?php $game->getSong1()->getWrong3()->printFull(); ?></div>
-<div id="4"><input id="r4" type="radio" name="guess" value="4" /><?php  $game->getSong1()->printFull(); ?></div>
+<input id="r1" type="radio" name="guess" value="1"/ ><span id="1"><?php $game->getSong1()->getWrong1()->printFull(); ?></span><br />
+<input id="r2" type="radio" name="guess" value="2" /><span id="2"><?php $game->getSong1()->getWrong2()->printFull(); ?></span> <br />
+<input id="r3" type="radio" name="guess" value="3" /><span id="3"><?php $game->getSong1()->getWrong3()->printFull(); ?></span> <br />
+<input id="r4" type="radio" name="guess" value="4" /><span id="4"><?php  $game->getSong1()->printFull(); ?></span>
 </div>
+
 
 </body>
 
 <script type='text/javascript'>//<![CDATA[ 
+var themovie=0;
+var correct=0;
 window.onload=function(){
 	jwplayer('player').setup({
 		flashplayer: 'http://player.longtailvideo.com/player.swf',
@@ -195,13 +195,64 @@ window.onload=function(){
 	jwplayer().onPlay( function() {  
 	var clock = $('.final_countdown .value');
 	Application.timer.countdown_from(clock, 10);
-//});
-	//jwplayer().onPause( function() { this.play(); });
-//jwplayer().onComplete( function() { 
+	if(themovie==4)
+	{
+	$("#1").text("Never Gonna Give You Up by Rick Astley");
+	$("#2").text("Never Gonna Give You Up by Rick Astley");
+	$("#3").text("Never Gonna Give You Up by Rick Astley");
+	$("#4").text("Never Gonna Give You Up by Rick Astley");
+	if(document.getElementById('r3').checked)
+	{
+		correct++;
+	}
+	//alert("you got "+correct+" correct!");
+	}
+	if(themovie==3)
+	{
+	$("#1").text("What Goes Around...Comes Around by Justin Timberlake");
+	$("#2").text("Sabotage by Kristinia DeBarge");
+	$("#3").text("Toxic by Britney Spears");
+	$("#4").text("Broken Heels by Alexandra Burke");
+	if(document.getElementById('r3').checked)
+	{
+		correct++;
+	}
+	}
+	if(themovie==2)
+	{
+	$("#1").text("Good Feeling by Flo Rida");
+	$("#2").text("Chopped N Skrewed by T-Pain");
+	$("#3").text("Single Again by Trina");
+	$("#4").text("400 Degreez by Juvenile");
+	if(document.getElementById('r1').checked)
+	{
+		correct++;
+	}
+	}
+	if(themovie==1)
+	{
+	$("#1").text("All I Want For Christmas Is You by Mariah Carey");
+	$("#2").text("Bad Romance by Lady Gaga");
+	$("#3").text("Hero by Mariah Carey");
+	$("#4").text("Cried Me A River by Kristinia DeBarge");
+	if(document.getElementById('r2').checked)
+	{
+		correct++;
+	}
+	}
+	if(themovie==0)
+	{
 	if(document.getElementById('r4').checked)
 	{
-		alert("you got it right");
-	}	});
+		correct++;
+	}
+	}
+		document.getElementById('r1').checked=0;
+		document.getElementById('r2').checked=0;
+		document.getElementById('r3').checked=0;
+		document.getElementById('r4').checked=0;
+	themovie++;
+	});
 }//]]>  
 	
 </script>
